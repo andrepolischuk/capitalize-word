b9ee4d5bada7094ed98ae4cbb2e9cd6b492f3925
Andrey Polischuk <andre.polischuk@gmail.com>
2016-08-14 21:58:10 +0300

    Add function name for error tracing

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 const regexp = /([A-Za-z\u00C0-\u1FFF\u2800-\uFFFD]+)/gi;
-export default input => input.replace(regexp, match => match.charAt(0).toUpperCase() + match.substr(1));
+
+export default function capitalizeWord(input) {
+  return input.replace(regexp, match => match.charAt(0).toUpperCase() + match.substr(1));
+}
